d103fcfb925987f6c04599fe3fc17cc0c8b3bd5d
NotNoelChannel <notnoel12345678@gmail.com>
2023-04-05 19:21:54 +0200

    X6C6X: More fixes for twrp-12.1

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -7,7 +7,6 @@
 
 LOCAL_PATH := device/lenovo/X6C6X
 
-PRODUCT_MAKEFILES := \ 
-		$(LOCAL_PATH)/twrp_X6C6X.mk
+PRODUCT_MAKEFILES := $(LOCAL_PATH)/twrp_X6C6X.mk
 
 COMMON_LUNCH_CHOICES := twrp_X6C6X-eng
