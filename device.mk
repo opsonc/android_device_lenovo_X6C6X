f1ba96e599d595de3f7ce6b69f9cd26dfcb67164
opsonc <58789337+opsonc@users.noreply.github.com>
2023-04-17 13:38:08 +0300

    Add files via upload

--- device.mk
+++ device.mk
@@ -1,11 +1,5 @@
-#
-# Copyright (C) 2023 The Android Open Source Project
-# Copyright (C) 2023 SebaUbuntu's TWRP device tree generator
-#
-# SPDX-License-Identifier: Apache-2.0
-#
-
 LOCAL_PATH := device/lenovo/X6C6X
+
 # A/B
 AB_OTA_POSTINSTALL_CONFIG += \
     RUN_POSTINSTALL_system=true \
@@ -16,12 +10,10 @@ AB_OTA_POSTINSTALL_CONFIG += \
 # Boot control HAL
 PRODUCT_PACKAGES += \
     android.hardware.boot@1.0-impl \
-    android.hardware.boot@1.0-service
-
-PRODUCT_PACKAGES += \
+    android.hardware.boot@1.0-service \
     bootctrl.mt6765
 
-PRODUCT_STATIC_BOOT_CONTROL_HAL := \
+PRODUCT_SHARED_LIBRARY := \
     bootctrl.mt6765 \
     libgptutils \
     libz \
